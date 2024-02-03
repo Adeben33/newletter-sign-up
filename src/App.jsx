18aa7647c807c4c700cd1848af4e191990a7f0f3
adeben33 <adeniyiebenezer33@gmail.com>
2024-02-02 19:41:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main>
+      <div>
+        <div className="heading">Stay updated!</div>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div></div>
+    </main>
+  );
+}
+
+export default App;
