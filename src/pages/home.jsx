322d5377e8a20670e219ccbd72c768eee523d522
adeben33 <adeniyiebenezer33@gmail.com>
2024-02-06 20:39:34 -0500

    react router

---
+++ src/pages/home.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import "./App.css";
+import isEmail from "validator/lib/isEmail";
+
+import { useHistory } from "react-router-dom";
+
+function Home() {
+  const [emailError, setEmailError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const validateEmail = (email) => {
+    return isEmail(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+    if (validateEmail(email)) {
+      setEmailError(false);
+    } else {
+      setEmailError(true);
+    }
+  };
+  return (
+    <main>
+      <div className="container">
+        <div className="heading">Stay updated!</div>
+        <p>
+          Join 60,000+ product managers receiving monthly <br /> updates on:
+        </p>
+        <ul>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21"
+              height="21"
+              viewBox="0 0 21 21"
+            >
+              <g fill="none">
+                <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+                <path
+                  stroke="#FFF"
+                  strokeWidth="2"
+                  d="M6 11.381 8.735 14 15 8"
+                />
+              </g>
+            </svg>
+            <p>Product discovery and building what matters </p>
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21"
+              height="21"
+              viewBox="0 0 21 21"
+            >
+              <g fill="none">
+                <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+                <path
+                  stroke="#FFF"
+                  strokeWidth="2"
+                  d="M6 11.381 8.735 14 15 8"
+                />
+              </g>
+            </svg>
+            <p>Measuring to ensure updates are a success</p>
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21"
+              height="21"
+              viewBox="0 0 21 21"
+            >
+              <g fill="none">
+                <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+                <path
+                  stroke="#FFF"
+                  strokeWidth="2"
+                  d="M6 11.381 8.735 14 15 8"
+                />
+              </g>
+            </svg>
+            <p>And much more!</p>
+          </li>
+        </ul>
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="email" className="label">
+            Email address
+            {emailError ? (
+              <span className="error"> Invalid email address</span>
+            ) : (
+              ""
+            )}
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email@company.com"
+            className={emailError && submitted ? "error-box" : ""}
+            required
+          />
+          <input
+            type="submit"
+            value="Subscribe to monthly newsletter"
+            onClick={handleSubmit}
+          />
+        </form>
+      </div>
+      <div className="image"></div>
+    </main>
+  );
+}
+
+export default App;
